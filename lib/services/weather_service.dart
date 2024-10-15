584f350ad2c19f42f2943f31e9c7a19da37ba44a
‪Amar Yasser‬‏ <amaryasser046@gmail.com>
2024-10-15 17:56:02 +0300

    Refactor weather service to handle API errors and exceptions

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,15 +1,28 @@
+import 'dart:developer';
+
 import 'package:dio/dio.dart';
 import 'package:weather_app/models/weather_model.dart';
 
 class WeatherService {
   final Dio dio;
+
+  final String baseUrl = 'https://api.weatherapi.com/v1';
+  final String apiKey = '9aded3fd36d54626a53175631242409';
   WeatherService(this.dio);
   Future<WeatherModel> getCurrentWeather({required String cityName}) async {
-    Response response = await dio.get(
-        'https://api.weatherapi.com/v1/forecast.json?key=9aded3fd36d54626a53175631242409&q=$cityName&days=1');
+    try {
+      Response response = await dio
+          .get('$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=1');
 
-    WeatherModel weatherModel = WeatherModel.fromJson(response.data);
-
-    return weatherModel;
+      WeatherModel weatherModel = WeatherModel.fromJson(response.data);
+      return weatherModel;
+    } on DioException catch (e) {
+      final String errMessage = e.response?.data['error']['message'] ??
+          'oops there is an error , try later';
+      throw Exception(errMessage);
+    } catch (e) {
+      log(e.toString());
+      throw Exception('oops there is an error , try laterop');
+    }
   }
 }
