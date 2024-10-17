872713086f3e90824ec21eb200bf469e62956f1d
‪Amar Yasser‬‏ <amaryasser046@gmail.com>
2024-10-17 22:42:32 +0300

    Refactor weather service to handle API errors and exceptions

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/views/search_view.dart';
 import 'package:weather_app/widgets/no_weather_body.dart';
+import 'package:weather_app/widgets/weather_info_body.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -26,7 +27,9 @@ class HomeView extends StatelessWidget {
           )
         ],
       ),
-      body: const NoWeatherBody(),
+      body: weatherModel == null
+          ? const NoWeatherBody()
+          : const WeatherInfoBody(),
     );
   }
 }
--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -1,6 +1,7 @@
-import 'dart:developer';
-
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/services/weather_service.dart';
 
 class SearchView extends StatelessWidget {
   const SearchView({super.key});
@@ -12,11 +13,13 @@ class SearchView extends StatelessWidget {
         title: const Text('Search City'),
       ),
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16),
+        padding: const EdgeInsets.symmetric(horizontal: 16),
         child: Center(
           child: TextField(
-            onSubmitted: (value) {
-              log(value);
+            onSubmitted: (value) async {
+              weatherModel = await WeatherService(Dio())
+                  .getCurrentWeather(cityName: value);
+              Navigator.pop(context);
             },
             decoration: const InputDecoration(
               contentPadding: EdgeInsets.symmetric(
@@ -36,3 +39,5 @@ class SearchView extends StatelessWidget {
     );
   }
 }
+
+WeatherModel? weatherModel;
